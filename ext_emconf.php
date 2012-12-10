006f8b2d46647ef3d338fdcae358f092e47214f8
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-10 16:55:43 +0000

    updated version number
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_smallads/trunk@68669 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "ke_smallads"
+# Extension Manager/Repository config file for ext "ke_smallads".
 #
-# Auto generated 09-06-2010 11:35
+# Auto generated 10-12-2012 17:54
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Submit, display and admin smallads (Classified advertisements, Announcements). FE Users can edit and delete ads they have submitted. Picture upload is possible. Easy to install.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.3',
+	'version' => '1.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"3008";s:10:"README.txt";s:4:"f248";s:12:"ext_icon.gif";s:4:"d83b";s:17:"ext_localconf.php";s:4:"3e2b";s:14:"ext_tables.php";s:4:"c8b2";s:14:"ext_tables.sql";s:4:"bf33";s:15:"flexform_ds.xml";s:4:"1620";s:31:"icon_tx_kesmallads_smallads.gif";s:4:"6dab";s:34:"icon_tx_kesmallads_smallads__f.gif";s:4:"64cd";s:35:"icon_tx_kesmallads_smallads__ft.gif";s:4:"36a0";s:34:"icon_tx_kesmallads_smallads__h.gif";s:4:"9495";s:34:"icon_tx_kesmallads_smallads__t.gif";s:4:"c469";s:34:"icon_tx_kesmallads_smallads__x.gif";s:4:"36a0";s:16:"locallang_db.php";s:4:"b966";s:7:"tca.php";s:4:"f82e";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"d770";s:14:"mod1/index.php";s:4:"e98e";s:18:"mod1/locallang.php";s:4:"be64";s:22:"mod1/locallang_mod.php";s:4:"7242";s:19:"mod1/moduleicon.gif";s:4:"0b86";s:30:"mod1/_notes/moduleicon.gif.mno";s:4:"8249";s:27:"images/noImageAvailable.gif";s:4:"160c";s:14:"doc/manual.sxw";s:4:"9fa9";s:19:"doc/wizard_form.dat";s:4:"b962";s:20:"doc/wizard_form.html";s:4:"cccc";s:23:"example/ke_smallads.css";s:4:"01b4";s:31:"pi1/class.tx_kesmallads_pi1.php";s:4:"8b8f";s:17:"pi1/locallang.php";s:4:"22c8";s:24:"pi1/static/editorcfg.txt";s:4:"b895";s:20:"pi1/static/setup.txt";s:4:"f068";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"3008";s:10:"README.txt";s:4:"f248";s:12:"ext_icon.gif";s:4:"d83b";s:17:"ext_localconf.php";s:4:"94ca";s:14:"ext_tables.php";s:4:"c8b2";s:14:"ext_tables.sql";s:4:"bf33";s:15:"flexform_ds.xml";s:4:"1620";s:31:"icon_tx_kesmallads_smallads.gif";s:4:"6dab";s:34:"icon_tx_kesmallads_smallads__f.gif";s:4:"64cd";s:35:"icon_tx_kesmallads_smallads__ft.gif";s:4:"36a0";s:34:"icon_tx_kesmallads_smallads__h.gif";s:4:"9495";s:34:"icon_tx_kesmallads_smallads__t.gif";s:4:"c469";s:34:"icon_tx_kesmallads_smallads__x.gif";s:4:"36a0";s:16:"locallang_db.php";s:4:"becc";s:7:"tca.php";s:4:"f82e";s:14:"doc/manual.sxw";s:4:"bac4";s:19:"doc/wizard_form.dat";s:4:"b962";s:20:"doc/wizard_form.html";s:4:"cccc";s:23:"example/ke_smallads.css";s:4:"6cad";s:27:"images/noImageAvailable.gif";s:4:"160c";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"d770";s:14:"mod1/index.php";s:4:"b326";s:18:"mod1/locallang.php";s:4:"281c";s:22:"mod1/locallang_mod.php";s:4:"7242";s:19:"mod1/moduleicon.gif";s:4:"0b86";s:30:"mod1/_notes/moduleicon.gif.mno";s:4:"8249";s:31:"pi1/class.tx_kesmallads_pi1.php";s:4:"4b0b";s:17:"pi1/locallang.php";s:4:"1bdd";s:24:"pi1/static/editorcfg.txt";s:4:"b895";s:20:"pi1/static/setup.txt";s:4:"aeb2";}',
 	'suggests' => array(
 	),
 );
